bc36f12995a2c9fceecce3978d06065010fab34e
James Quick <james.quick@fedex.com>
2017-09-14 08:59:18 -0500

    Added navbar sample

--- Navbar/app.js
+++ Navbar/app.js
@@ -1,12 +1,11 @@
 let currentTabIndex = 1;
 
 function select(index){
-    console.log("selected " + index);
-    console.log("this", this);
     if(currentTabIndex !== index){
+        //Get previous and new active tabs
         const previouslySelected = document.querySelector(`.nav-items .nav-item:nth-child(${currentTabIndex})`);
         const newlySelected = document.querySelector(`.nav-items .nav-item:nth-child(${index})`);
-        console.log(previouslySelected);
+        //Update classes
         previouslySelected.classList.remove("active");
         newlySelected.classList.add("active");
         currentTabIndex = index;
